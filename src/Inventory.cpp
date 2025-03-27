6a8bce06379793bc2a345ed5f2d835c2134cd088
st129351 <st129351@student.spbu.ru>
2025-03-27 21:54:52 +0300

    fix warning from ci

--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -43,7 +43,7 @@ void Inventory::add(std::shared_ptr<Amulet> item)
 
 void Inventory::remove(std::shared_ptr<Amulet> item)
 {
-    for (int i = 0; i < amulets.size(); i++)
+    for (size_t i = 0; i < amulets.size(); i++) // vector.size() usually return "size_t" type
     {
         if (amulets[i] == item)
         {
